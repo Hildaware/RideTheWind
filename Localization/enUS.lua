2efb7f79222cbeed115fe733bd77c80b752cb56b
Brandon Ferrari <ferrari.l.brandon@gmail.com>
2024-02-01 15:08:29 -0500

    Added Localization files

---
+++ Localization/enUS.lua
@@ -0,0 +1,8 @@
+local addonName = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "enUS", true)
+
+L['Normal'] = true
+L['Advanced'] = true
+L['Reverse'] = true
+L['Challenge'] = true
+L['Challenge Reverse'] = true
